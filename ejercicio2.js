2efa14f5080be502cd5b42a9a3c3a9c741b2b2cb
Lautaro Quispe <112778818+tpudv2@users.noreply.github.com>
2022-09-19 23:38:17 -0300

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,15 @@
+//MODULO 2 - Ejercicio de lista
+
+const lista = [
+    "Lautaro",
+    25,
+    true,
+    new Date(1997, 04, 06),
+    { 
+        titulo: "Si! De acuerdo",
+        autor: "Roger Fisher",
+        fecha: new Date(1993, 0, 0),
+        url: "https://books.google.com.ar/books/about/Si_de_acuerdo.html?id=bGfKwAEACAAJ&source=kp_book_description&redir_esc=y"
+    },
+];
+
