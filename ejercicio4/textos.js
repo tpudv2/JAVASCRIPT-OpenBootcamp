da5f39de92453e72324364c73019072356effe92
Lautaro Quispe <112778818+tpudv2@users.noreply.github.com>
2022-09-22 13:51:42 -0300

    Add files via upload

---
+++ ejercicio4/textos.js
@@ -0,0 +1,19 @@
+
+// 
+
+const nombre = "Lautaro"
+const apellido = "Quispe"
+const estudiante = nombre.concat(" ").concat(apellido)
+
+const estudianteMayus = estudiante.toUpperCase()
+const estudianteMinus = estudiante.toLowerCase()
+
+const estudianteLength = estudiante.length
+
+const inicialNombre = nombre.substring(0, 1)
+
+const finalApellido = apellido.substring(apellido.length - 1, apellido.length)
+
+const estudianteSinEspacios = estudiante.replace(/ /g, "")
+
+const nombreEnEstudiante = estudiante.includes(nombre)
